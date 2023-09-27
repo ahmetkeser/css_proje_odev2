dd9585bbbc5f33d6ce17e75973f6a8e52e8ede5b
ahmetkeser <58327575+ahmetkeser@users.noreply.github.com>
2023-09-27 17:55:00 +0300

    template literals çalışması

--- deneme.js
+++ deneme.js
@@ -1,13 +1,22 @@
-let userName1="ahmet"
-let userName2="keser"
-const DOMAIN="bilgihavuzu.org"
-let email = userName1+userName2+"@"+DOMAIN
+let firstname ="Ahmet"
+let lastname ="KESER"
+const DOMAIN="denemejs.com"
+const DOMAIN2=DOMAIN.replace('denemejs','gmail') // mevcut domain adresinden başka bir adres türetildi
+let email=firstname.toLowerCase() + lastname.toLowerCase() + "@" + DOMAIN //Kullanıcı isim soyisim ile domain uzantılı e  mail üret
 console.log(email)
+console.log(`              
+kullanıcı ${firstname} sana tahsis edilen ${DOMAIN} adresiniz
+${email}
+`) //^Template Literals kullanımını pekiştir^
 console.log(`
-${userName1} hoşgeldin. sana tahsis edilen mail :${userName1+userName2+"@"+DOMAIN}
+${DOMAIN.slice(0,DOMAIN.indexOf("."),).toUpperCase()} Tarafından 
+
+${DOMAIN.slice(DOMAIN.indexOf("."),)} uzantılı adresiniz değiştirmek isterseniz 
+
+seçenekler
+* ${lastname.toLowerCase()+firstname.toLowerCase()+"@"+DOMAIN}
+** ${firstname.toLocaleLowerCase()+lastname.toLowerCase()+"@"+DOMAIN2}
+***${lastname.toLowerCase()+firstname.toLowerCase()+"@"+DOMAIN2}
+****${firstname[0].toLowerCase()+lastname.toLowerCase()+"@"+DOMAIN2}
+*****${firstname.toLowerCase()+lastname[0].toLowerCase()+"@"+DOMAIN2}
 `)
-console.log(userName1.length)//harf sayısı
-let first =userName1[0].toUpperCase()+userName2[0].toLocaleUpperCase()
-console.log(first)
-console.log("aradığım:",userName1.search("ezzt")) //-1 sonucu döndürür
-console.log("aradığım:",userName2.search("ser"))
